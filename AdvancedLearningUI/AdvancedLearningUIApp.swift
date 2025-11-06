7321d7f4961291d1597de8a075bb7bc891ca2b59
Tirzaan Neil <Tirzaan@tntneil.com>
2025-11-06 15:37:59 -0700

    Initial Commit

---
+++ AdvancedLearningUI/AdvancedLearningUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedLearningUIApp.swift
+//  AdvancedLearningUI
+//
+//  Created by Tirzaan on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedLearningUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
