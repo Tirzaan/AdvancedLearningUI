d55359addeddc7cd96aec44c457bd1026418470f
Tirzaan Neil <Tirzaan@tntneil.com>
2025-11-19 21:20:44 -0700

    [Episode] 9 - View Builder

---
+++ AdvancedLearningUI/Lessons 1-10/ViewBuilderLesson.swift
@@ -0,0 +1,18 @@
+//
+//  ViewBuilderLesson.swift
+//  AdvancedLearningUI
+//
+//  Created by Tirzaan on 11/12/25.
+//
+
+import SwiftUI
+
+struct ViewBuilderLesson: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewBuilderLesson()
+}
