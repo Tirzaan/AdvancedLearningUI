1f1e828149089763daf3e53c59779f249d0a311b
Tirzaan Neil <Tirzaan@tntneil.com>
2025-11-19 21:27:37 -0700

    [Episode] 9 - Continued

--- AdvancedLearningUI/ContentView.swift
+++ AdvancedLearningUI/ContentView.swift
@@ -7,19 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MGIImage: View {
+    let category: String
+    let imageName: String
+    let aspectContentMode: ContentMode = .fill
+    
     var body: some View {
-        NavigationStack {
-            List {
-                NavigationLink("Custom View Modifiers") {
-                    ViewModifierLesson()
-                }
-                
-                NavigationLink("Custom Button Styles") {
-                    ButtonStyleLesson()
-                }
+        AsyncImage(url: URL(string: "https://firebasestorage.googleapis.com/v0/b/fun-with-languages.firebasestorage.app/o/Images%2FMatchingGame%2F\(category)%2FMGI-945-\(imageName).png?alt=media&token=2c503f6f-d1e0-4415-a8ab-b9d2d5be608c")) { image in
+            if let image = image.image {
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: aspectContentMode)
             }
         }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+//        NavigationStack {
+//            List {
+//                NavigationLink("Custom View Modifiers") {
+//                    ViewModifierLesson()
+//                }
+//                
+//                NavigationLink("Custom Button Styles") {
+//                    ButtonStyleLesson()
+//                }
+//            }
+//        }
+        
+        MGIImage(category: "Animals", imageName: "Fish")
+            .frame(width: 400, height: 400)
     }
 }
 
--- AdvancedLearningUI/Lessons 1-10/ViewBuilderLesson.swift
+++ AdvancedLearningUI/Lessons 1-10/ViewBuilderLesson.swift
@@ -7,12 +7,156 @@
 
 import SwiftUI
 
+struct HeaderViewRegular: View {
+    let title: String
+    let description: String?
+    let iconName: String?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            if let description {
+                Text(description)
+                    .font(.callout)
+            }
+            
+            if let iconName {
+                Image(systemName: iconName)
+            }
+            
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height: 2)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
+struct HeaderViewGeneric<Content: View>: View {
+    let title: String
+    let content: Content
+    
+    init(title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            content
+            
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height: 2)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
+struct CustomHStack<Content: View>: View {
+    let content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        HStack {
+            content
+        }
+    }
+}
+
+struct LocalViewBuilder: View {
+    enum ViewType {
+        case one
+        case two
+        case three
+    }
+    let type: ViewType
+    
+    var body: some View {
+        VStack {
+            header
+        }
+    }
+    
+    @ViewBuilder
+    private var header: some View {
+        switch type {
+        case .one:
+            viewOne
+        case .two:
+            viewTwo
+        case .three:
+            viewThree
+        }
+        
+//        if type == .one {
+//            viewOne
+//        } else if type == .two {
+//            viewTwo
+//        } else if type == .three {
+//            viewThree
+//        }
+    }
+    
+    private var viewOne: some View {
+        Text("one")
+    }
+    
+    private var viewTwo: some View {
+        VStack {
+            Text("TWOOO")
+            Image(systemName: "arrowshape.left.fill")
+        }
+    }
+    
+    private var viewThree: some View {
+        Image(systemName: "space")
+    }
+}
+
 struct ViewBuilderLesson: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderViewRegular(title: "New Title", description: "hello", iconName: "heart.fill")
+            HeaderViewRegular(title: "Other Title", description: nil, iconName: nil)
+            
+            HeaderViewGeneric(title: "Generic Title") {
+                Text("Hello")
+            }
+            
+            HeaderViewGeneric(title: "Generic Two") {
+                Image(systemName: "sparkles")
+            }
+            
+            HeaderViewGeneric(title: "Generic Three") {
+                HStack {
+                    Text("One")
+                    Image(systemName: "bolt.fill")
+                    Text("Three")
+                }
+            }
+            
+            CustomHStack {
+                Text("One")
+                Image(systemName: "bolt.fill")
+                Text("Three")
+            }
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    ViewBuilderLesson()
+    LocalViewBuilder(type: .one)
 }
