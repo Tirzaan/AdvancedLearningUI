5bbc1a002dd67e408dce0d55e28446fcc41e6ae8
Tirzaan Neil <Tirzaan@tntneil.com>
2026-01-09 22:24:32 -0700

    [Episode] 18 - UI Tsting

---
+++ AdvancedLearningUI/Lessons 11-20/UITestingLessonView.swift
@@ -0,0 +1,18 @@
+//
+//  UITestingLessonView.swift
+//  AdvancedLearningUI
+//
+//  Created by Tirzaan on 1/7/26.
+//
+
+import SwiftUI
+
+struct UITestingLessonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UITestingLessonView()
+}
