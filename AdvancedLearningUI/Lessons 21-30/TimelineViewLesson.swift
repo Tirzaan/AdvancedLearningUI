5ab91e319932bf80d86e73f9db2a6e9e30b6f83f
Tirzaan Neil <Tirzaan@tntneil.com>
2026-01-16 22:55:17 -0700

    [Feature] TimelineView Code

--- AdvancedLearningUI/Lessons 21-30/TimelineViewLesson.swift
+++ AdvancedLearningUI/Lessons 21-30/TimelineViewLesson.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct TimelineViewLesson: View {
+    
+    @State var startTime: Date = .now
+    @State var isPaused: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TimelineView(.animation(minimumInterval: 1, paused: isPaused)) { context in
+                Text("\(context.date)")
+                Text("\(context.date.timeIntervalSince1970)")
+                
+//                let seconds = Calendar.current.component(.second, from: context.date)
+                let seconds = context.date.timeIntervalSince(startTime)
+                Text("\(seconds)")
+                
+                if context.cadence == .live {
+                    Text("Cadence: Live")
+                } else if context.cadence == .minutes {
+                    Text("Cadence: Minutes")
+                } else if context.cadence == .seconds {
+                    Text("Cadence: Seconds")
+                }
+                
+                Rectangle()
+                    .frame(width: seconds < 10 ? 50 : seconds < 30 ? 200 : 400, height: 100)
+                    .animation(.spring(bounce: 0.5), value: seconds)
+            }
+            
+            Button("Pause / Resume") {
+                isPaused.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 10))
+            .padding()
+        }
     }
 }
 
