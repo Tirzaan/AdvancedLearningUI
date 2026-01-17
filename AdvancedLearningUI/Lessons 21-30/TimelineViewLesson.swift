f0f1753d3085749f9544cbdc45f5952a185feb0d
Tirzaan Neil <Tirzaan@tntneil.com>
2026-01-16 22:54:16 -0700

    [Episode] 26 - TimelineView

---
+++ AdvancedLearningUI/Lessons 21-30/TimelineViewLesson.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineViewLesson.swift
+//  AdvancedLearningUI
+//
+//  Created by Tirzaan on 1/16/26.
+//
+
+import SwiftUI
+
+struct TimelineViewLesson: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimelineViewLesson()
+}
